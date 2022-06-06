600b81b9d566dbea0f8997feb08675de435d589c
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-06-06 17:05:12 +0530

    Button Role & Click Event

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />)
-  const linkElement = screen.getByText('Learn React')
-  expect(linkElement).toBeInTheDocument()
+
+  // Find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+
+  // Expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  // Click button
+  fireEvent.click(colorButton)
+
+  // Expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+
+  // Expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red')
 })
