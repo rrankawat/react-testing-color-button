81282477216cf144f08dd192a81c0b41aa3b0d16
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-08-10 17:18:34 +0530

    Button Click

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
 test('button has correct initial color', () => {
@@ -9,6 +9,13 @@ test('button has correct initial color', () => {
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
-})
 
-// test('button turns blue when clicked', () => {})
+  // click button
+  fireEvent.click(colorButton)
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red')
+})
